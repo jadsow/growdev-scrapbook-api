ddca2a5697893399910ec81971e08edf2e922c28
Jadson Pereira <p.jadsonn@gmail.com>
2022-02-22 11:05:17 -0300

    backend longe do fim

--- src/index.ts
+++ src/index.ts
@@ -32,20 +32,28 @@ app.post('/cadastro', (request: Request, response: Response) => {
 
 const listaDeRecados: any = []
 
-app.post('/listar-recado', (request: Request, response: Response) => {
+app.post('/adicionar-recado', (request: Request, response: Response) => {
     const {prioridade, recado} = request.body
 
     const recados = {
+        id: Math.floor(Math.random() * 100),
         prioridade,
         recado,
     }
 
     listaDeRecados.push(recados)
-    return response.status(200).json({
-        mensagem: 'Recado foi registrado'
-    })
-
+    
+    return response.status(200).json(recados)
 })
 
+app.delete('/deletar-recado/:id', (request: any, response: any) => {
+    const {id} = request.params;
+    const index = listaDeRecados.findIndex(recados => recados.id == parseInt(id));
+
+    listaDeRecados.splice(index,1);
+
+    return response.sendStatus(204)
+
+})
 
 app.listen(8080, () => console.log('Api em funcionamento...'))
