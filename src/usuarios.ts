260bdec35a0a10b10bc23795e3d9b0ca240919a1
Jadson Pereira <p.jadsonn@gmail.com>
2022-02-23 10:20:31 -0300

    back-end envoluindo

--- src/usuarios.ts
+++ src/usuarios.ts
@@ -1,5 +1,8 @@
+import Recados from "./recados";
+
 export default class Usuario {
     id: number;
+    recados: Recados[] = []
     constructor (public nome: string,
                 private senha: string) {
         this.id = Math.floor(Math.random() * 100);
